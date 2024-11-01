0d7db8304699f929dbb717390a5771982ba05e66
projectsbymj <71979478+projectsbymj@users.noreply.github.com>
2024-11-01 12:28:48 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from wad_data import WADData
+from settings import *
+import pygame as pg
+import sys
+from map_renderer import MapRenderer
+from player import Player
+from bsp import BSP
+
+
+class DoomEngine:
+    def __init__(self, wad_path ='wad/DOOM1.wad'):
+        self.wad_path = wad_path
+        self.screen = pg.display.set_mode(WIN_RES)
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.dt = 1/60
+        self.on_init()
+    def on_init(self):
+        self.wad_data = WADData(self, map_name='E1M1')
+        self.map_renderer = MapRenderer(self)
+        self.player = Player(self)
+        self.bsp = BSP(self)
+
+    def update(self):
+        self.player.update()
+        self.bsp.update()
+        self.dt = self.clock.tick()
+        pg.display.set_caption(f'{self.clock.get_fps()}')
+
+    def draw(self):
+        pg.display.flip()
+        self.screen.fill('black')
+        self.map_renderer.draw()
+
+
+    def check_events(self):
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                self.running = False
+
+    def run(self):
+        while self.running:
+            self.check_events()
+            self.update()
+            self.draw()
+        pg.quit()
+
+
+if __name__ == '__main__':
+    doom = DoomEngine()
+    doom.run()
